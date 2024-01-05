9e58048f2a68f2ecb4430d8b196cff0682bd6d47
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-05 20:28:59 +0300

    caches and assembly files

--- VbApi/Vb.Data/obj/Debug/net7.0/Vb.Data.AssemblyInfo.cs
+++ VbApi/Vb.Data/obj/Debug/net7.0/Vb.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vb.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7979ba201127f8ee2b968795abb6f8ca63f74da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vb.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vb.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
