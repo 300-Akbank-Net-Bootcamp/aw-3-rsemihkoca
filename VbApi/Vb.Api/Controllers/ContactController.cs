dde18b9ff420f062a8f7d930282c17167623b5ce
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-05 20:25:41 +0300

    Contact

--- VbApi/Vb.Api/Controllers/ContactController.cs
+++ VbApi/Vb.Api/Controllers/ContactController.cs
@@ -1,9 +1,67 @@
+
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Vb.Base.Response;
+using Vb.Business.Cqrs;
+using Vb.Schema;
 
 namespace VbApi.Controllers;
 
-[NonController]
-public class ContactController
+[Route("api/[controller]")]
+[ApiController]
+public class ContactController : ControllerBase
 {
+    private readonly IMediator mediator;
+    public ContactController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet("All")]
+    public async Task<ApiResponse<List<ContactResponse>>> Get()
+    {
+        var operation = new GetAllContactQuery();
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ApiResponse<ContactResponse>> Get(int id)
+    {
+        var operation = new GetContactByIdQuery(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
     
+    [HttpGet]
+    public async Task<ApiResponse<List<ContactResponse>>> Get(string contactType, string information, int isDefault) 
+    {
+        var operation = new GetContactByParameterQuery(contactType, information, isDefault);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+    
+    [HttpPost]
+    public async Task<ApiResponse<ContactResponse>> Post([FromBody] ContactRequest customer)
+    {
+        var operation = new CreateContactCommand(customer);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ApiResponse> Put(int id, [FromBody] ContactRequest customer)
+    {
+        var operation = new UpdateContactCommand(id,customer);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ApiResponse> Delete(int id)
+    {
+        var operation = new DeleteContactCommand(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
 }
